352187779dcca971c5ca6c48ecbbdc0a83f7b5dc
Ayan <ayanchhipa2278@gmail.com>
2024-12-26 17:07:29 +0530

    Sign in And signup

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,12 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from "../../constants";
+import FormField from '../../components/FormField';
+import CustomButton from "../../components/CustomButton"
+import { Link } from 'expo-router';
 
 const SignUp = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
+    const [form, setForm] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const submit = ()=>{
+        alert('Please enter');
+    }
+    return (
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView>
+                <View className="w-full justify-center min-h-[83vh] px-4 my-6">
+                    <Image
+                        source={images.logo}
+                        resizeMode='contain'
+                        className="w-[115px] h-[35px]"
+                    />
+                    <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">SignUp to Aura</Text>
+                    <FormField
+                        title="Username"
+                        value={form.username}
+                        handleChangeText={(e) => setForm({ ...form, username: e })}
+                        otherStyles="mt-10"
+                    />
+                    <FormField
+                        title="Email"
+                        value={form.email}
+                        handleChangeText={(e) => setForm({ ...form, email: e })}
+                        otherStyles="mt-7"
+                        keyboardType="email-address"
+                    />
+
+                    <FormField
+                        title="Password"
+                        value={form.password}
+                        handleChangeText={(e) => setForm({ ...form, password: e })}
+                        otherStyles="mt-7"
+                    />
+                    <CustomButton
+                    title="Sign In"
+                    handlePress={submit}
+                    containerStyle="mt-7"
+                    isLoading={isSubmitting}
+                    />
+                    <View className="justify-center pt-5 flex-row gap-2">
+                        <Text className="text-lg text-gray-100 font-pregular">
+                            Have an account already? <Link href="/sign-in" className='text-lg font-psemibold text-secondary'>Sign In</Link>
+                        </Text>
+
+                    </View>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default SignUp
+
+const styles = StyleSheet.create({})
--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { Image, ScrollView, Text, View } from "react-native";
 import "../global.css";
-import { Link, SplashScreen } from "expo-router";
+import { Link, SplashScreen, Redirect, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants"
@@ -11,7 +11,7 @@ export default function Index() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full h-full px-4 items-center">
+        <View className="w-full h-full px-4 items-center min-h-[85px] px-4">
           <Image source={images.logo} className="w-[130px] h-[83px]" resizeMode="contain" />
           <Image source={images.cards} className="max-w--[380px] w-full h-[300px]" resizeMode="contain" />
           <View className="relative mt-5">
@@ -27,7 +27,7 @@ export default function Index() {
 
           </View>
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center"> Where creativity meets innovation: embark on a journey of limitless exploration with Aura!</Text>
-          <CustomButton title="Continue with Email" handlePress={()=>{}} containerStyle="w-full mt-7"/>
+          <CustomButton title="Continue with Email" handlePress={()=>{ router.push("/sign-in")}} containerStyle="w-full mt-7"/>
         </View>
       </ScrollView>
       <StatusBar backgroundColor="#161622" style="light"/>
